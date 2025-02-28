e3d562bf44e5868e8a022aaa013a65879a2176ca
Liv Lau <77578448+Livlau@users.noreply.github.com>
2025-02-28 10:43:25 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,20 @@
+const searchInput = document.getElementById(searchInput).value;
+const input = searchInput.trim();
+
+const btnSearch = document.getElementById("btnSearch");
+btnSearch.addEventListener("click", search);
+
+const btnClear = document.getElementById("btnClear");
+btnClear.addEventListener("click", clear);
+
 function thankyou() {
   alert('Thank you for contacting us!')
+}
+
+function search() {
+  alert("search");
+}
+
+function clear() {
+  searchInput = ""; 
 }
